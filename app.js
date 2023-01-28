bf7c9549941bdd793c0948a2bcd01cbfab7ae962
divay805 <divay805@gmail.com>
2023-01-28 18:20:10 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+ 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Mongoose
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/todoListData');
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+  
+// // Schema is created 
+// => Ist Step 
+const itmeSchema = {
+  name:String
+
+}
+
+// // Then model is created 
+const Item = mongoose.model("Item",itmeSchema);
+// // 
+
+// // Mongoose doucment is created below
+const goToLab =  new Item({
+  name:"goToWork"
+})
+
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
